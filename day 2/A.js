4a561daf18df64f22aec3eae0f785f607666a6ec
OK007-code <ok007@example.com>
2026-07-31 11:48:55 +0530

    functions

--- day 2/A.js
+++ day 2/A.js
@@ -1,3 +1,16 @@
-console.log("===== Variables =====");
-var text="Something";
-console.log(text);
+console.log("\nPercentage");
+
+function totalMarks(math, english, science) {
+    return math + english + science;
+}
+
+function percentage(total) {
+    return (total / 300) * 100;
+}
+
+var total = totalMarks(80, 75, 90);
+var percent = percentage(total);
+
+console.log("Total Marks:", total);
+console.log("Percentage:", percent + "%");
+
