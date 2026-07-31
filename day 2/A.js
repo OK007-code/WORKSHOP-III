ea4b78e9650a0f4811ac64a7c6a81e85860a32f4
OK007-code <ok007@example.com>
2026-07-31 11:27:38 +0530

    Add day 2 script

--- day 2/A.js
+++ day 2/A.js
@@ -1,17 +1,3 @@
-// console.log("===== Variables =====");
-// var text="Something";
-// console.log(text);
-
-// var a=40;
-// var b=50;
-// if(a>b){
-//     console.log("a is greater than b, value of a is: "+a);
-// }else{
-//     console.log("b is greater than a, value of b is: "+b);
-// }
-console.log("===== Functions =====");
-function abes(){
-    console.log("This is a function");
-    console.log("welcome to abes");
-}
-abes();
+console.log("===== Variables =====");
+var text="Something";
+console.log(text);
