ca756c57f4d53b9a6e438742c09bb319c677a276
OK007-code <ok007@example.com>
2026-07-31 10:54:40 +0530

    Add day1 workshop files

---
+++ day1/app.js
@@ -0,0 +1,4 @@
+module.exports = {
+    x2: 20,
+    y2: 30
+};
---
+++ day1/index.js
@@ -0,0 +1,23 @@
+const app = require('./app.js');
+// var a=20;
+// var b=30;
+// var c=40;
+// console.log(a+b+c);
+
+// var x=20;
+// if(x==20){
+//     console.log("x is 20");
+// }
+
+// for(var i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// var a=20;
+// const arr=[10,20,30,40,50];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+console.log(app.x2);
+console.log(app.y2);
