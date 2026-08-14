4a415f620c5bc312f2d899357637c9bf17c79982
Omika <145779808+OK007-code@users.noreply.github.com>
2026-08-14 12:06:52 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,63 @@
+//task1-even or odd
+// function checkNumber(num) {
+//     if (num % 2 === 0) {
+//         return "Even";
+//     }
+//     else {
+//         return "Odd";
+//     }
+// }
+// console.log(checkNumber(4));
+// console.log(checkNumber(7)); 
+
+//task2- student details
+// function getStudentDetails(name, id, department) {
+//     return {
+//         name: name,
+//         id: id,
+//         department: department
+//     };
+// }
+
+// let student1 = getStudentDetails("Alice", 2024, "Aiml");
+// let student2 = getStudentDetails("Bob", 2025, "cse");
+
+// console.log(student1);
+// console.log(student2);
+
+//task3- calculate student percentage
+// function calculatePercentage(marks) {
+//     let totalMarks = 0;
+//     for (let i = 0; i < marks.length; i++) {
+//         totalMarks += marks[i];
+//     }
+//     let percentage = (totalMarks / (marks.length * 100)) * 100;
+//     console.log(percentage);
+// }
+
+// let students=[
+//     {name:"Alice", marks:[85, 90, 78]},
+//     {name:"Bob", marks:[92, 88, 95]},
+//     {name:"Charlie", marks:[75, 80, 70]}
+// ];
+// console.log("Student Percentages:");
+// for(let i=0;i<students.length;i++){
+//     let student=students[i];
+//     let totalMarks=0;
+//     for(let j=0;j<student.marks.length;j++){
+//         totalMarks+=student.marks[j];
+//     }
+//     let percentage=(totalMarks/(student.marks.length*100))*100;
+//     console.log(student.name+": "+percentage+"%");
+// }
+
+const std={
+    name:"Alice",
+    id: 2024,
+    department: "Aiml",
+    marks: [85, 90, 78]
+}
+console.log("Student Name:", std.name);
+console.log("Student ID:", std.id);
+console.log("Student Department:", std.department);
+console.log("Student Marks:", std.marks);
