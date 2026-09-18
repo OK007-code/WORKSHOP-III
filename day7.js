477f74204f4860e6fc527ae3eeb77c14c2c7bbf1
OK007-code <ok007@example.com>
2026-09-18 11:49:48 +0530

    3

--- day7.js
+++ day7.js
@@ -18,23 +18,55 @@
 // obj2.show();
 
 //create a class Sec A and display data of 5 students using contructor
-class Student {
-    constructor(name, age, rollNo) {
-        this.name = name;
-        this.age = age;
-        this.rollNo = rollNo;
+// class Student {
+//     constructor(name, age, rollNo) {
+//         this.name = name;
+//         this.age = age;
+//         this.rollNo = rollNo;
+//     }
+//     show() {
+//         console.log("Name: " + this.name + ", Age: " + this.age + ", Roll No: " + this.rollNo);
+//     }
+// }
+// let student1 = new Student("Omika", 20, 1);
+// let student2 = new Student("Jivisha", 22, 2);
+// let student3 = new Student("Saanvi", 21, 3);
+// let student4 = new Student("Kavish", 23, 4);
+// let student5 = new Student("BoomBoom", 20, 5);
+// student1.show();
+// student2.show();
+// student3.show();
+// student4.show();
+// student5.show();
+
+class hello{
+    static name="example of static method with instance var"
+    constructor(){
+        this.instanceVar="example of instance var"
     }
-    show() {
-        console.log("Name: " + this.name + ", Age: " + this.age + ", Roll No: " + this.rollNo);
+    show(){
+        console.log("Static Var: " + hello.name);
+        console.log("Instance Var: " + this.instanceVar);
     }
 }
-let student1 = new Student("Omika", 20, 1);
-let student2 = new Student("Jivisha", 22, 2);
-let student3 = new Student("Saanvi", 21, 3);
-let student4 = new Student("Kavish", 23, 4);
-let student5 = new Student("BoomBoom", 20, 5);
-student1.show();
-student2.show();
-student3.show();
-student4.show();
-student5.show();
+
+let obj = new hello();
+obj.show();
+//static method with instance method without constructor
+class Student {
+    name;
+    roll_number;
+    static college_name="ABESEC";
+    show() {
+        console.log("Name: " + this.name + ", Roll Number: " + this.roll_number);
+    }
+    static displayCollegeName() {
+        console.log("College Name: " + this.college_name);
+    }
+}
+const student = new Student();
+student.name = "Omika";
+student.roll_number = 131;
+
+student.show();
+Student.displayCollegeName();
